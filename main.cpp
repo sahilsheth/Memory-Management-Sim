ca763f983b3f28628c1292f13adc16c0db1e6454
cesmartz10 <36048368+cesmartz10@users.noreply.github.com>
2019-12-03 22:39:41 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@
 //Prompt user for name of a workload file( Has int N = # of processes in file)
 //Each process has unique process id first line, Arrival time, Lifetime in Memory
 // and Address space seperated by single blank line
+#include <stdio.h>
+#include <stdbool.h>
+
+__intptr_t currBreak; //Global variable that tracks processes
+// Used allocate more memory by incrementing break value
+void *sbrk(__intptr_t inc){ //From Stackoverflow.com
+  __intptr_t oldBreak = currBreak;
+  currBreak += inc;
+  return (void*) oldBreak;
+}
+
 typedef struct mem_block{
   struct mem_block *nxt; //Points to next memory
   bool free; // Checks if memory block is free
@@ -20,14 +31,29 @@ _MEMBLOCK *memBlockAllocation(size_t size){
   void *memAllocation = (void*)sbrk(BLOCK_SIZE + size); //Changes location with respect to Block size
 
   // Checks if memory allocation pointer can increment
-  if(memAllocation == (*void)-1) {
+  if(memAllocation == (void*) -1){
     return NULL;
   }
   else{
     b->nxt = NULL;
     b->free = false;
     b->size = size;
-    b->memAddress = memAddy + BLOCK_SIZE;
+    b->memAddress = memAddy + BLOCK_SIZE; //ERROR HERE, MEANT TO INCREMENT BLOCK
     return b;
+  }
+}
+
+// Allocates next memory block
+void nextAllocationMemBlock(size_t size, _MEMBLOCK ** head){
+
+}
+
+void freeMemoryBlock(_MEMBLOCK **head){
+
+  if(*head == NULL){
+    //Memory is not free
+  }
+  else{//Overwrites contents
+    (*head)->free = true;
   }
 }
